3f4fc6f41474c0da8956f859ff803b5f2c716949
Israel-Mege <isralmege@gmail.com>
2025-03-08 18:44:27 -0300

    feat: agregar configuración para despliegue en Cloudflare Pages

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
@@ -10,4 +12,8 @@ const nextConfig = {
   }
 };
 
-module.exports = nextConfig;
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
+
+export default nextConfig;
